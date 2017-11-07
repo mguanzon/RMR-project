fde441fb3b74ddcfb9798dc7a35fa3c40754622c
Melissa Guanzon <mguanzon@ucsd.edu>
2017-11-07 13:54:09 -0800

    added candidates page

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 /********************** HOME PAGE ******************************/
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}*/
+
+
+
+
 
 /********************* EDIT PAGE (ADD POSITION) ***********************/
 function addPosToDropDown() {
@@ -27,5 +13,4 @@ function addPosToDropDown() {
   var option= document.createElement('option');
   option.text = document.getElementById('posName').value;
   x.options.add(option);
-
 }
