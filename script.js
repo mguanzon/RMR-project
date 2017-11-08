8e89eb10c1af28fc68e1362ab1323a356e7c09c7
Melissa Guanzon <mguanzon@ucsd.edu>
2017-11-07 23:04:13 -0800

    help

--- script.js
+++ script.js
@@ -4,12 +4,25 @@
 function addPosToDropDown() {
   var x = document.getElementById("pos1");
   var option= document.createElement("option");
-  /*var text = document.getElementById("posName").value;*/
-  var text = getValue(posName);
+  var text = document.getElementById("posName").value;
+  /*var text = getValue(posName);*/
   option.text = text;
   x.add(option);
 }
 
+/* function processForm()
+  {
+    var parameters = location.search.substring(1).split("&");
+    var temp = parameters[0].split("=");
+    l = unescape(temp[1]);
+    alert(l); //Dialog with the text you put on the textbox
+  }
+  processForm();*/
+
+
+
+
 $("#search").click(function() {
-	var newName = 
-})
+	$("#pos1").addPosToDropDown();
+
+});
