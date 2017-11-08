9e3de58d9e8d69e0ed0e9a6c37e3ca4741c0ab44
Melissa Guanzon <mguanzon@ucsd.edu>
2017-11-07 19:39:07 -0800

    edits

--- script.js
+++ script.js
@@ -9,8 +9,9 @@
 
 /********************* EDIT PAGE (ADD POSITION) ***********************/
 function addPosToDropDown() {
-  var x = document.getElementById('pos1');
-  var option= document.createElement('option');
-  option.text = document.getElementById('posName').value;
-  x.options.add(option);
+  var x = document.getElementById("pos1");
+  var option= document.createElement("option");
+  var text = document.getElementById("posName").value;
+  option.text = text;
+  x.add(option);
 }
